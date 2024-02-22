fad40f81c73f94e80034baec1647b255df132339
Daram0o0 <c30728535@gmail.com>
2024-02-22 21:28:31 +0900

    first commit'

---
+++ day2-FoodMenu/script.js
@@ -0,0 +1,69 @@
+const itemlist = {
+  item: "분식류",
+  menu: [
+    {
+      name: "떡볶이",
+      price: 5000,
+      description: "쌀/밀 떡볶이"
+    },
+    {
+      name: "순대",
+      price: 6000,
+      description: "내장은 없어요ㅠㅠ"
+    },
+    {
+      name: "어묵",
+      price: 3000,
+      description: "4개에 3000원"
+    },
+  ],
+  item: "김밥류",
+  menu: [
+    {
+      name: "기본김밥",
+      price: 4000,
+      description: "기본에 충실한 김밥"
+    },
+    {
+      name: "참치김밥",
+      price: 4500,
+      description: "이달의 인기메뉴"
+    },
+    {
+      name: "땡초김밥",
+      price: 4500,
+      description: "꽤나 매워요"
+    },
+  ],
+  item: "면류",
+  menu: [
+    {
+      name: "어묵우동",
+      price: 6000,
+      description: "참치김밥이랑 베스트 조합"
+    },
+    {
+      name: "땡초라면",
+      price: 5000,
+      description: "땡초 원하는 만큼 넣어드려요"
+    }
+  ],
+  item: "음료",
+  menu: [
+    {
+      name: "콜라",
+      price: 2000,
+      description: "제로는 취급 안 합니다"
+    },
+    {
+      name: "사이다",
+      price: 2000,
+      description: "제로는 취급 안 합니다"
+    },
+    {
+      name: "물",
+      price: 1000,
+      description: "매장에서 먹으면 공짜"
+    }
+  ],
+};
